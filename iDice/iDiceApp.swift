41c90e698e20b87a0219fe1f6a290b7fc6a6e27b
Víctor Ávila <victoraavila2000@gmail.com>
2024-10-07 00:24:31 -0300

    Initial Commit

---
+++ iDice/iDiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDiceApp.swift
+//  iDice
+//
+//  Created by Víctor Ávila on 07/10/24.
+//
+
+import SwiftUI
+
+@main
+struct iDiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
