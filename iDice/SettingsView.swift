397542b317fcb14e4e23b99a20bd26c6e5419e7c
Víctor Ávila <victoraavila2000@gmail.com>
2024-10-07 01:38:49 -0300

    Initial SettingsView defined

--- iDice/SettingsView.swift
+++ iDice/SettingsView.swift
@@ -8,8 +8,38 @@
 import SwiftUI
 
 struct SettingsView: View {
+    @State private var numberOfSides = 6
+    @State private var numberOfDice = 1
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Form {
+            Section {
+                HStack {
+                    Spacer() // Align to the left
+                    Picker("", selection: $numberOfSides) {
+                        Text("4").tag(4)
+                        Text("6").tag(6)
+                        Text("8").tag(8)
+                        Text("10").tag(10)
+                        Text("12").tag(12)
+                        Text("20").tag(20)
+                        Text("100").tag(100)
+                    }
+                    .pickerStyle(.segmented) // Optional for better style
+                    Spacer() // Align to the right
+                }
+            } header: {
+                Text("Number of sides on each die")
+            }
+            
+            Section {
+                Stepper(value: $numberOfDice, in: 1...8, step: 1) {
+                    Text(numberOfDice == 1 ? "\(numberOfDice) die" : "\(numberOfDice) dice")
+                }
+            } header: {
+                Text("Number of dice")
+            }
+        }
     }
 }
 
