636bf3902c20e0bb1d2e31b7d8555cdd98aff250
Víctor Ávila <victoraavila2000@gmail.com>
2024-10-07 01:29:48 -0300

    Add roll Button

--- iDice/ContentView.swift
+++ iDice/ContentView.swift
@@ -10,22 +10,55 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         NavigationStack {
-            DieView()
-                .toolbar {
-                    ToolbarItem(placement: .navigationBarLeading) {
-                        Text("iDice")
-                            .font(.largeTitle)
-                            .bold()
+            GeometryReader { geometry in
+                VStack {
+                    Spacer()
+                    
+                    HStack {
+                        Spacer()
+                        
+                        DieView()
+                            .frame(width: geometry.size.width * 0.8, height: geometry.size.width * 0.8)
+                        
+                        Spacer()
                     }
                     
-                    ToolbarItem(placement: .navigationBarTrailing) {
-                        NavigationLink(destination: SettingsView()) {
-                            Image(systemName: "gearshape.circle.fill")
-                                .foregroundStyle(.gray)
-                                .font(.title)
-                        }
+                    Spacer()
+                    
+                    Button() {
+
+                    } label: {
+                        Text("ROLL")
+                            .bold()
+                            .kerning(5.0)
+                            .foregroundStyle(Color.white)
+                            .shadow(radius: 10)
+                            .frame(width: 300, height: 70)
+                            .background(
+                                RoundedRectangle(cornerRadius: 10.0)
+                                    .foregroundStyle(.gray.opacity(0.5))
+                            )
+                    }
+                    
+                    Spacer()
+                }
+                .frame(maxWidth: .infinity, maxHeight: .infinity) // Ensures VStack takes all available space
+            }
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Text("iDice")
+                        .font(.largeTitle)
+                        .bold()
+                }
+                
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    NavigationLink(destination: SettingsView()) {
+                        Image(systemName: "gearshape.circle.fill")
+                            .foregroundStyle(.gray)
+                            .font(.title)
                     }
                 }
+            }
         }
     }
 }
@@ -33,4 +66,5 @@ struct ContentView: View {
 #Preview {
     ContentView()
 }
+
 
